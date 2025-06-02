d7a52d3eaad392d7beea48bad1d6df4d9da415c2
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-06-02 09:52:20 -0400

    deferred execution

--- S25Week5IntroToLinq/Program.cs
+++ S25Week5IntroToLinq/Program.cs
@@ -23,6 +23,33 @@
             foreach (var i in greaterThan5)
                 Console.Write(i + " ");
             Console.WriteLine("\n\n");
+
+
+
+            List<string> colors = new List<string>();
+            colors.Add("bLuE");
+            colors.Add("rUSt");
+            colors.Add("GreEn");
+            colors.Add("ReD");
+
+            var startsWithR = from c in colors
+                              let uppercaseStr = c.ToUpper()
+                              where uppercaseStr.StartsWith("R")
+                              orderby uppercaseStr
+                              select uppercaseStr;
+
+            foreach (var i in startsWithR)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+
+
+            colors.Add("rUBy");
+            colors.Add("PiNk");
+
+            // deferred execution
+            foreach (var i in startsWithR)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
         }
     }
 }
