ccc3a8e4861d0973a0e7a0ff5c279a5c2f60a07f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-06-02 09:32:52 -0400

    linq

--- S25Week5IntroToLinq/Program.cs
+++ S25Week5IntroToLinq/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 4, 2, 5, 7, 6, 5, 8, 9, 1, 4, 5, 7, 3 };
+
+            var greaterThan4 = from num in array
+                               where num > 4
+                               select num;
+
+            foreach (var i in greaterThan4)
+                Console.Write(i + " ");
+            Console.WriteLine("\n\n");
         }
     }
 }
