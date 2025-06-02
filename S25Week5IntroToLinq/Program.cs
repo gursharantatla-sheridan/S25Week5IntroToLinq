377e09de563cedce75175da63c9192e5b153b722
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-06-02 09:41:02 -0400

    linq

--- S25Week5IntroToLinq/Program.cs
+++ S25Week5IntroToLinq/Program.cs
@@ -6,11 +6,21 @@
         {
             int[] array = { 4, 2, 5, 7, 6, 5, 8, 9, 1, 4, 5, 7, 3 };
 
+            // query syntax
             var greaterThan4 = from num in array
                                where num > 4
+                               orderby num
                                select num;
 
             foreach (var i in greaterThan4)
+                Console.Write(i + " ");
+            Console.WriteLine("\n\n");
+
+
+            // method syntax
+            var greaterThan5 = array.Where(n => n > 5).OrderBy(n => n).ToList();
+
+            foreach (var i in greaterThan5)
                 Console.Write(i + " ");
             Console.WriteLine("\n\n");
         }
